6bdef096289584cd48064515e35c32e14af2b4a8
KenDaryn <62947094+KenDaryn@users.noreply.github.com>
2020-04-06 12:16:27 +0600

    Add files via upload

--- area_triangle.rb
+++ area_triangle.rb
@@ -2,6 +2,5 @@ puts "Please write the base of the triangle"
 base = gets.chomp.to_f
 puts "Please write the height of the triangle"
 height = gets.chomp.to_f
-a = 1.0/2.0
-area = a * base * height 
+area = 0.5 * base * height 
 puts "Hello! The area of ​​the triangle is flat #{area}"
